20599312d1f841464169a330f8f30c080c2a0c76
mervemepa <mervemepa@users.noreply.github.com>
2024-02-28 09:58:01 +0000

    draft file edited

--- draft.js
+++ draft.js
@@ -10,14 +10,10 @@ class Draft {
     //this.letter = letter;
     this.passiveColor = "#ffffff";
     this.activeColor = "#000000";
-    //this.passiveColor = "#29220A";
-    //this.activeColor = "#ffffff";
     this.char = char;
     this.state = 0; //for color control
     this.debugEnabled = 0;
-    //noStroke();
     textAlign(CENTER, CENTER);
-    //ellipseMode(CORNER);
   }
 
   isClicked(px, py) {
@@ -58,19 +54,9 @@ class Draft {
 
   show() {
     noStroke();
-    //strokeWeight(0.5);
-    //fill(0);
-    //rect(this.x, this.y, this.w, this.h);
-    //fill(this.cl);
     fill(this.cl);
-
     rect(this.x, this.y, this.w - 2.5, this.h - 2.5);
-    //ellipse(this.x, this.y, this.w, this.h);
-    //fill(0);
-    //kutuların içindeki rakamları kapattık
-    /* */
     if (this.debugEnabled == 1) {
-      //fill(0);
       textSize(7);
       text(this.char, this.x + this.w * 0.5, this.y + this.h * 0.5);
     }
